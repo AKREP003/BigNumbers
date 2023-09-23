6114a35d0b61fa064b9e21c814c9451b0ef2cb72
AKREP003 <59123551+AKREP003@users.noreply.github.com>
2023-09-23 18:30:55 +0300

    mmm

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod veg;
+mod meth_with_more_digits;
+
+use std::ops::Add;
+use std::rc::Rc;
+use veg::vegg::*;
+
+
+
+fn main() {
+
+    let mut u = vec![0; 2];
+
+    u.push(1);
+
+    dbg!(u);
+
+}
